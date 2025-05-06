53dd34c24d88864b2cf71b0ff23ffe9973031bd2
Madvait28 <a.advait.menon@gmail.com>
2025-05-05 22:45:28 -0500

    compiled files

--- fish.c
+++ fish.c
@@ -1,6 +1,5 @@
 #include "variables.h"
 extern PetscReal CMx_c,CMy_c,CMz_c;
-// Advait Testing GITPUSH
 //extern PetscInt ti,tistart;
 extern PetscInt eel,pizza,moveframe;
 extern PetscReal St_exp,wavelength;
